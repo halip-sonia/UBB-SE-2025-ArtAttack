a00d832fcdd4808a288585b61db7bb26c4f54e2e
Darius Timoce <74290441+MadeOfDye@users.noreply.github.com>
2025-03-19 00:06:07 +0200

    Runda 2 la Baza de date de la Contracte

---
+++ ArtAttack/Database/Entities/PredefinedContract.sql
@@ -0,0 +1,7 @@
+--drop table [PredefinedContract]
+
+create table [PredefinedContract](
+	[ID] integer identity primary key,
+	[content] TEXT not null,
+	--contract type should be expunged from Class Diagram as it is held by the Contract Parent Table
+);
