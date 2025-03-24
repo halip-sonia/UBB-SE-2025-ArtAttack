50527e42b7dfddcb4391c2e461a9e6ed433ae52a
Darius Timoce <74290441+MadeOfDye@users.noreply.github.com>
2025-03-24 22:47:28 +0200

    Updated the Model and procedures for the change to the product, and created the Dummy product model(might be useless)

---
+++ ArtAttack/Domain/DummyProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ArtAttack.Domain
+{
+    public class DummyProduct
+    {
+        public int ID { get; set; }
+        public float Price { get; set; }
+        public int SellerID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
