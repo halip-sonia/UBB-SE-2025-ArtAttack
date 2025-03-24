502387600771db753f648924ac58ca2e0f179fbe
Moldovan-Stefan-Iustin <146836085+Moldovan-Stefan-Iustin@users.noreply.github.com>
2025-03-24 22:20:59 +0200

    Recreate NotificationCategory.cs ca nu stiu unde o disparut lol

---
+++ ArtAttack/Domain/NotificationCategory.cs
@@ -0,0 +1,15 @@
+namespace ArtAttack.Domain
+{
+    public enum NotificationCategory
+    {
+        CONTRACT_EXPIRATION,
+        OUTBIDDED,
+        ORDER_SHIPPING_PROGRESS,
+        PRODUCT_AVAILABLE,
+        PAYMENT_CONFIRMATION,
+        PRODUCT_REMOVED,
+        CONTRACT_RENEWAL_REQ,
+        CONTRACT_RENEWAL_ANS,
+        CONTRACT_RENEWAL_WAITLIST,
+    }
+}
