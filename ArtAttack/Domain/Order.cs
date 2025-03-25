c14e1f735f85f11cba13f8a82727c4581fff04ab
Bogdy09 <138328776+Bogdy09@users.noreply.github.com>
2025-03-25 23:34:08 +0200

    Order model and domain

---
+++ ArtAttack/Domain/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtAttack.Domain
+{
+    internal class Order
+    {
+        public int OrderID { get; set; }
+
+        public int ProductID { get; set; }
+        public int BuyerID { get; set; }
+        public string ProductType { get; set; }  
+        public int OrderSummaryID { get; set; }
+
+        public string PaymentMethod { get; set; }
+        public DateTime OrderDate { get; set; }
+
+       
+        public virtual DummyProduct Product { get; set; }
+        public virtual DummyBuyer Buyer { get; set; }
+        public virtual OrderSummary OrderSummary { get; set; }
+    }
+}
---
+++ ArtAttack/Domain/OrderSummary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtAttack.Domain
+{
+    internal class OrderSummary
+    {
+        public int ID { get; set; }  
+
+        public double Subtotal { get; set; }
+        public double WarrantyTax { get; set; }
+        public double DeliveryFee { get; set; }
+        public double FinalTotal { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string AdditionalInfo { get; set; }
+        public string ContractDetails { get; set; }  
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
