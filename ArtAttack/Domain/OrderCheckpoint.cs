b2071ba415cc22fa0e588984d78a0f2e650ebb15
ilincamares <ilinca.mares@stud.ubbcluj.ro>
2025-03-25 22:17:34 +0200

    Implemented the Domain and the Model classes for TrackedOrder section

---
+++ ArtAttack/Domain/OrderCheckpoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtAttack.Domain
+{
+    enum OrderStatus
+    {
+        PROCESSING,
+        SHIPPED,
+        IN_WAREHOUSE,
+        IN_TRANSIT,
+        OUT_FOR_DELIVERY,
+        DELIVERED
+    }
+
+    class OrderCheckpoint
+    {
+        public int CheckpointID { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string? Location { get; set; }
+        public string Description { get; set; }
+        public OrderStatus Status { get; set; }
+        public int TrackedOrderID { get; set; }
+
+    }
+}
