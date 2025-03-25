835a06a796ae7289f948f7edc577b90e5425c553
Moldovan-Stefan-Iustin <146836085+Moldovan-Stefan-Iustin@users.noreply.github.com>
2025-03-25 23:40:14 +0200

    Create NotificationDataAdapter.cs

---
+++ ArtAttack/Model/NotificationDataAdapter.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iss.Domain;
+
+namespace ArtAttack.Model
+{
+    public class NotificationDataAdapter : IDisposable
+    {
+        private readonly SqlConnection _connection;
+
+        public NotificationDataAdapter(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
+        }
+
+        public List<Notification> GetNotificationsForUser(int recipientId)
+        {
+            var notifications = new List<Notification>();
+            const string query = @"
+            SELECT * FROM Notification 
+            WHERE recipientID = @RecipientId
+            ORDER BY timestamp DESC";
+
+            using (var command = new SqlCommand(query, _connection))
+            {
+                command.Parameters.AddWithValue("@RecipientId", recipientId);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        notifications.Add(NotificationFactory.CreateFromDataReader(reader));
+                    }
+                }
+            }
+            return notifications;
+        }
+
+        public void MarkAsRead(int notificationId)
+        {
+            const string query = @"
+            UPDATE Notification 
+            SET isRead = 1 
+            WHERE notificationID = @NotificationId";
+
+            using (var command = new SqlCommand(query, _connection))
+            {
+                command.Parameters.AddWithValue("@NotificationId", notificationId);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Dispose()
+        {
+            _connection?.Close();
+            _connection?.Dispose();
+        }
+    }
+}
