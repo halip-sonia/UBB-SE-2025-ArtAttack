e13d926c6fc2ee1242e90bcf1a6a2c2397c748ab
Sonia Halip <147024654+halip-sonia@users.noreply.github.com>
2025-03-25 19:13:03 +0200

    Update DummyProductModel.cs

--- ArtAttack/Model/DummyProductModel.cs
+++ ArtAttack/Model/DummyProductModel.cs
@@ -17,15 +17,17 @@ namespace ArtAttack.Model
             _connectionString = connectionString;
         }
 
-        public void AddDummyProduct(float price, int sellerId, DateTime startDate, DateTime endDate)
+        public void AddDummyProduct(string name, float price, int sellerId, string productType, DateTime startDate, DateTime endDate)
         {
             using (SqlConnection conn = new SqlConnection(_connectionString))
             {
                 using (SqlCommand cmd = new SqlCommand("AddDummyProduct", conn))
                 {
                     cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Name", name);
                     cmd.Parameters.AddWithValue("@Price", price);
                     cmd.Parameters.AddWithValue("@SellerID", sellerId);
+                    cmd.Parameters.AddWithValue("@ProductType", productType);
                     cmd.Parameters.AddWithValue("@StartDate", startDate);
                     cmd.Parameters.AddWithValue("@EndDate", endDate);
 
@@ -38,7 +40,7 @@ namespace ArtAttack.Model
         /// <summary>
         /// Updates an existing DummyProduct record.
         /// </summary>
-        public void UpdateDummyProduct(int id, float price, int sellerId, DateTime startDate, DateTime endDate)
+        public void UpdateDummyProduct(int id, string name, float price, int sellerId, string productType, DateTime startDate, DateTime endDate)
         {
             using (SqlConnection conn = new SqlConnection(_connectionString))
             {
@@ -46,8 +48,10 @@ namespace ArtAttack.Model
                 {
                     cmd.CommandType = CommandType.StoredProcedure;
                     cmd.Parameters.AddWithValue("@ID", id);
+                    cmd.Parameters.AddWithValue("@Name", name);
                     cmd.Parameters.AddWithValue("@Price", price);
                     cmd.Parameters.AddWithValue("@SellerID", sellerId);
+                    cmd.Parameters.AddWithValue("@ProductType", productType);
                     cmd.Parameters.AddWithValue("@StartDate", startDate);
                     cmd.Parameters.AddWithValue("@EndDate", endDate);
 
