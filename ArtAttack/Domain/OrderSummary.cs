c84282dc2ddacdef0991dd8ffeb18d5a9202b97b
Bogdy09 <138328776+Bogdy09@users.noreply.github.com>
2025-03-27 00:33:02 +0200

    Update OrderSummary.cs

--- ArtAttack/Domain/OrderSummary.cs
+++ ArtAttack/Domain/OrderSummary.cs
@@ -10,10 +10,10 @@ namespace ArtAttack.Domain
     {
         public int ID { get; set; }  
 
-        public double Subtotal { get; set; }
-        public double WarrantyTax { get; set; }
-        public double DeliveryFee { get; set; }
-        public double FinalTotal { get; set; }
+        public float Subtotal { get; set; }
+        public float WarrantyTax { get; set; }
+        public float DeliveryFee { get; set; }
+        public float FinalTotal { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
