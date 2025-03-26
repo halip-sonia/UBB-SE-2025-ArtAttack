2b4be125008f5206804ef33f4555b75da5ad821b
halip-sonia <sonia.halip@stud.ubbcluj.ro>
2025-03-26 21:30:32 +0200

    added dummy domains

---
+++ ArtAttack/Services/PaymentDetailValidator.cs
@@ -0,0 +1,58 @@
+﻿using ArtAttack.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtAttack.Services
+{
+    public class PaymentDetailValidator
+    {
+        public bool ValidateCardNumber(string cardNumber)
+        {
+            // luhn algorithm implementation for card number validation
+            int numberOfDigits = cardNumber.Length;
+
+            if (!cardNumber.All(char.IsAsciiDigit) || numberOfDigits!=16)
+                return false;
+
+            int nSum = 0;
+            bool isSecond = false; 
+            for (int i = numberOfDigits - 1; i >= 0; i--)
+            {
+                int d = cardNumber[i] - '0';
+
+                if (isSecond == true)
+                    d = d * 2;
+
+                // adding in case the doubling results in a 2 digit number
+                nSum += d / 10;
+                nSum += d % 10;
+
+                isSecond = !isSecond;
+            }
+            return (nSum % 10 == 0);
+        }
+
+        public bool ValidateCVC(string cvc)
+        {
+            return (cvc.All(char.IsAsciiDigit) && cvc.Length == 3);
+        }
+
+        public bool ValidateMonth(string month)
+        {
+            return (month.All(char.IsAsciiDigit) && month.Length == 2);
+        }
+
+        public bool ValidateYear(string year)
+        {
+            return (year.All(char.IsAsciiDigit) && year.Length == 2);
+        }
+
+        //public bool ValidateCardDetails()
+        //{
+        //   
+        //}
+    }
+}
