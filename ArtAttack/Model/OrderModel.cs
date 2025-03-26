e4ac0ac9ca52584978a3de7a32e8f29dc64f2262
Sonia Halip <147024654+halip-sonia@users.noreply.github.com>
2025-03-26 02:04:36 +0200

    added ordermodel and ordersummarymodel

---
+++ ArtAttack/Model/OrderModel.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtAttack.Model
+{
+    internal class OrderModel
+    {
+        private readonly string _connectionString;
+
+        public OrderModel(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void AddOrder(int productId, int buyerId, string productType, string paymentMethod, int orderSummaryId, DateTime orderDate)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("AddOrder", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ProductID", productId);
+                    cmd.Parameters.AddWithValue("@BuyerID", buyerId);
+                    cmd.Parameters.AddWithValue("@ProductType", productType);
+                    cmd.Parameters.AddWithValue("@PaymentMethod", paymentMethod);
+                    cmd.Parameters.AddWithValue("@OrderSummaryID", orderSummaryId);
+                    cmd.Parameters.AddWithValue("@OrderDate", orderDate);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void UpdateOrder(int orderId, string productType, string paymentMethod, DateTime orderDate)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("UpdateOrder", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@OrderID", orderId);
+                    cmd.Parameters.AddWithValue("@ProductType", productType);
+                    cmd.Parameters.AddWithValue("@PaymentMethod", paymentMethod);
+                    cmd.Parameters.AddWithValue("@OrderDate", orderDate);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteOrder(int orderId)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("DeleteOrder", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@OrderID", orderId);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<BorrowedOrderHistory> GetBorrowedOrderHistory(int buyerId)
+        {
+            List<BorrowedOrderHistory> orders = new List<BorrowedOrderHistory>();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("get_borrowed_order_history", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@BuyerID", SqlDbType.Int).Value = buyerId;
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            BorrowedOrderHistory order = new BorrowedOrderHistory
+                            {
+                                OrderID = reader.GetInt32("OrderID"),
+                                ProductID = reader.GetInt32("ProductID"),
+                                ProductType = reader.GetString("ProductType"),
+                                PaymentMethod = reader.GetString("PaymentMethod"),
+                                OrderDate = reader.GetDateTime("OrderDate"),
+                                Subtotal = reader.GetDecimal("Subtotal"),
+                                WarrantyTax = reader.GetDecimal("WarrantyTax"),
+                                DeliveryFee = reader.GetDecimal("DeliveryFee"),
+                                FinalTotal = reader.GetDecimal("finalTotal"),
+                                Address = reader.GetString("address"),
+                                AdditionalInfo = reader["AdditionalInfo"] as string,
+                                ContractDetails = reader["ContractDetails"] as string,
+                                ProductName = reader.GetString("ProductName")
+                            };
+                            orders.Add(order);
+                        }
+                    }
+                }
+            }
+            return orders;
+        }
+
+        public List<NewOrUsedOrderHistory> GetNewOrUsedOrderHistory(int buyerId)
+        {
+            List<NewOrUsedOrderHistory> orders = new List<NewOrUsedOrderHistory>();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("get_new_or_used_order_history", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@BuyerID", SqlDbType.Int).Value = buyerId;
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            NewOrUsedOrderHistory order = new NewOrUsedOrderHistory
+                            {
+                                OrderID = reader.GetInt32("OrderID"),
+                                ProductID = reader.GetInt32("ProductID"),
+                                ProductType = reader.GetString("ProductType"),
+                                PaymentMethod = reader.GetString("PaymentMethod"),
+                                OrderDate = reader.GetDateTime("OrderDate"),
+                                Subtotal = reader.GetDecimal("Subtotal"),
+                                DeliveryFee = reader.GetDecimal("DeliveryFee"),
+                                FinalTotal = reader.GetDecimal("finalTotal"),
+                                Address = reader.GetString("address"),
+                                AdditionalInfo = reader["AdditionalInfo"] as string,
+                                ProductName = reader.GetString("ProductName")
+                            };
+                            orders.Add(order);
+                        }
+                    }
+                }
+            }
+            return orders;
+        }
+
+
+        public List<OrderHistory> GetOrdersFromLastThreeMonths(int buyerId)
+        {
+            List<OrderHistory> orders = new List<OrderHistory>();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("get_orders_from_last_3_months", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@BuyerID", SqlDbType.Int).Value = buyerId;
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            OrderHistory order = new OrderHistory
+                            {
+                                OrderID = reader.GetInt32("OrderID"),
+                                ProductID = reader.GetInt32("ProductID"),
+                                ProductType = reader.GetString("ProductType"),
+                                PaymentMethod = reader.GetString("PaymentMethod"),
+                                OrderDate = reader.GetDateTime("OrderDate"),
+                                Subtotal = reader.GetDecimal("Subtotal"),
+                                WarrantyTax = reader.GetDecimal("WarrantyTax"),
+                                DeliveryFee = reader.GetDecimal("DeliveryFee"),
+                                FinalTotal = reader.GetDecimal("finalTotal"),
+                                Address = reader.GetString("address"),
+                                AdditionalInfo = reader["AdditionalInfo"] as string,
+                                ContractDetails = reader["ContractDetails"] as string,
+                                ProductName = reader.GetString("ProductName")
+                            };
+                            orders.Add(order);
+                        }
+                    }
+                }
+            }
+            return orders;
+        }
+
+        public List<OrderHistory> GetOrdersFromLastSixMonths(int buyerId)
+        {
+            List<OrderHistory> orders = new List<OrderHistory>();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("get_orders_from_last_6_months", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@BuyerID", SqlDbType.Int).Value = buyerId;
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            OrderHistory order = new OrderHistory
+                            {
+                                OrderID = reader.GetInt32("OrderID"),
+                                ProductID = reader.GetInt32("ProductID"),
+                                ProductType = reader.GetString("ProductType"),
+                                PaymentMethod = reader.GetString("PaymentMethod"),
+                                OrderDate = reader.GetDateTime("OrderDate"),
+                                Subtotal = reader.GetDecimal("Subtotal"),
+                                WarrantyTax = reader.GetDecimal("WarrantyTax"),
+                                DeliveryFee = reader.GetDecimal("DeliveryFee"),
+                                FinalTotal = reader.GetDecimal("finalTotal"),
+                                Address = reader.GetString("address"),
+                                AdditionalInfo = reader["AdditionalInfo"] as string,
+                                ContractDetails = reader["ContractDetails"] as string,
+                                ProductName = reader.GetString("ProductName")
+                            };
+                            orders.Add(order);
+                        }
+                    }
+                }
+            }
+            return orders;
+        }
+
+        public List<OrderHistory> GetOrdersFrom2025(int buyerId)
+        {
+            List<OrderHistory> orders = new List<OrderHistory>();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("get_orders_from_2025", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@BuyerID", SqlDbType.Int).Value = buyerId;
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            OrderHistory order = new OrderHistory
+                            {
+                                OrderID = reader.GetInt32("OrderID"),
+                                ProductID = reader.GetInt32("ProductID"),
+                                ProductType = reader.GetString("ProductType"),
+                                PaymentMethod = reader.GetString("PaymentMethod"),
+                                OrderDate = reader.GetDateTime("OrderDate"),
+                                Subtotal = reader.GetDecimal("Subtotal"),
+                                WarrantyTax = reader.GetDecimal("WarrantyTax"),
+                                DeliveryFee = reader.GetDecimal("DeliveryFee"),
+                                FinalTotal = reader.GetDecimal("finalTotal"),
+                                Address = reader.GetString("address"),
+                                AdditionalInfo = reader["AdditionalInfo"] as string,
+                                ContractDetails = reader["ContractDetails"] as string,
+                                ProductName = reader.GetString("ProductName")
+                            };
+                            orders.Add(order);
+                        }
+                    }
+                }
+            }
+            return orders;
+        }
+
+        public List<OrderHistory> GetOrdersFrom2024(int buyerId)
+        {
+            List<OrderHistory> orders = new List<OrderHistory>();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("get_orders_from_2024", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@BuyerID", SqlDbType.Int).Value = buyerId;
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            OrderHistory order = new OrderHistory
+                            {
+                                OrderID = reader.GetInt32("OrderID"),
+                                ProductID = reader.GetInt32("ProductID"),
+                                ProductType = reader.GetString("ProductType"),
+                                PaymentMethod = reader.GetString("PaymentMethod"),
+                                OrderDate = reader.GetDateTime("OrderDate"),
+                                Subtotal = reader.GetDecimal("Subtotal"),
+                                WarrantyTax = reader.GetDecimal("WarrantyTax"),
+                                DeliveryFee = reader.GetDecimal("DeliveryFee"),
+                                FinalTotal = reader.GetDecimal("finalTotal"),
+                                Address = reader.GetString("address"),
+                                AdditionalInfo = reader["AdditionalInfo"] as string,
+                                ContractDetails = reader["ContractDetails"] as string,
+                                ProductName = reader.GetString("ProductName")
+                            };
+                            orders.Add(order);
+                        }
+                    }
+                }
+            }
+            return orders;
+        }
+
+        public List<OrderHistory> GetOrdersByName(int buyerId, string text)
+        {
+            List<OrderHistory> orders = new List<OrderHistory>();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("get_orders_by_name", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@BuyerID", SqlDbType.Int).Value = buyerId;
+                    cmd.Parameters.Add("@text", SqlDbType.NVarChar, 250).Value = text;
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            OrderHistory order = new OrderHistory
+                            {
+                                OrderID = reader.GetInt32("OrderID"),
+                                ProductID = reader.GetInt32("ProductID"),
+                                ProductType = reader.GetString("ProductType"),
+                                PaymentMethod = reader.GetString("PaymentMethod"),
+                                OrderDate = reader.GetDateTime("OrderDate"),
+                                Subtotal = reader.GetDecimal("Subtotal"),
+                                WarrantyTax = reader.GetDecimal("WarrantyTax"),
+                                DeliveryFee = reader.GetDecimal("DeliveryFee"),
+                                FinalTotal = reader.GetDecimal("finalTotal"),
+                                Address = reader.GetString("address"),
+                                AdditionalInfo = reader["AdditionalInfo"] as string,
+                                ContractDetails = reader["ContractDetails"] as string,
+                                ProductName = reader.GetString("ProductName")
+                            };
+                            orders.Add(order);
+                        }
+                    }
+                }
+            }
+            return orders;
+        }
+
+    }
+}
---
+++ ArtAttack/Model/OrderSummaryModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtAttack.Model
+{
+    internal class OrderSummaryModel
+    {
+        private readonly string _connectionString;
+
+        public OrderSummaryModel(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void AddOrderSummary(float subtotal, float warrantyTax, float deliveryFee, float finalTotal,
+                                    string fullName, string email, string phoneNumber, string address,
+                                    string postalCode, string additionalInfo, string contractDetails)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("AddOrderSummary", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Subtotal", subtotal);
+                    cmd.Parameters.AddWithValue("@WarrantyTax", warrantyTax);
+                    cmd.Parameters.AddWithValue("@DeliveryFee", deliveryFee);
+                    cmd.Parameters.AddWithValue("@FinalTotal", finalTotal);
+                    cmd.Parameters.AddWithValue("@FullName", fullName);
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+                    cmd.Parameters.AddWithValue("@Address", address);
+                    cmd.Parameters.AddWithValue("@PostalCode", postalCode);
+                    cmd.Parameters.AddWithValue("@AdditionalInfo", additionalInfo);
+                    cmd.Parameters.AddWithValue("@ContractDetails", contractDetails ?? (object)DBNull.Value);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void UpdateOrderSummary(int id, float subtotal, float warrantyTax, float deliveryFee, float finalTotal,
+                                       string fullName, string email, string phoneNumber, string address,
+                                       string postalCode, string additionalInfo, string contractDetails)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("UpdateOrderSummary", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ID", id);
+                    cmd.Parameters.AddWithValue("@Subtotal", subtotal);
+                    cmd.Parameters.AddWithValue("@WarrantyTax", warrantyTax);
+                    cmd.Parameters.AddWithValue("@DeliveryFee", deliveryFee);
+                    cmd.Parameters.AddWithValue("@FinalTotal", finalTotal);
+                    cmd.Parameters.AddWithValue("@FullName", fullName);
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+                    cmd.Parameters.AddWithValue("@Address", address);
+                    cmd.Parameters.AddWithValue("@PostalCode", postalCode);
+                    cmd.Parameters.AddWithValue("@AdditionalInfo", additionalInfo);
+                    cmd.Parameters.AddWithValue("@ContractDetails", contractDetails ?? (object)DBNull.Value);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteOrderSummary(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("DeleteOrderSummary", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ID", id);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
