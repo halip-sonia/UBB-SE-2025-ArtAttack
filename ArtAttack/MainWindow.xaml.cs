184ebc7f47e00e29b5cbbd88ba4addb4bcb34b0f
Darius Timoce <74290441+MadeOfDye@users.noreply.github.com>
2025-03-27 04:51:37 +0200

    Created the button with data binding to the view model on the main page. Subject to being moved

--- ArtAttack/MainWindow.xaml.cs
+++ ArtAttack/MainWindow.xaml.cs
@@ -13,28 +13,35 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using ArtAttack.Domain;
+using ArtAttack.ViewModel;
+using ArtAttack.Shared;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace ArtAttack
-{ 
+{
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainWindow : Window
     {
-        Contract contract;
+
+        private Contract contract;
+        private IContractViewModel _contractViewModel;
+
         public MainWindow()
         {
             this.InitializeComponent();
             contract = new Contract();
+            _contractViewModel = new ContractViewModel(Configuration._CONNECTION_STRING_);
+            //example contract
+            contract = _contractViewModel.GetContractById(1);
         }
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
             myButton.Content = "Clicked";
-            
         }
 
         private void purchaseButton_Click(object sender, RoutedEventArgs e)
@@ -43,6 +50,19 @@ namespace ArtAttack
             var bp = new BillingInfo();
             billingInfoWindow.Content = bp;
             billingInfoWindow.Activate();
+        }
+
+        private void generateContractButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (contract != null)
+            {
+                _contractViewModel.GenerateAndSaveContract(contract);
+                //show a message to the user indicating the PDF was saved.
+            }
+            else
+            {
+                // Handle the error appropriately if the contract is null.
+            }
         }
     }
 }
