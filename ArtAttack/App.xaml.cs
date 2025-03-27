cff0f91e578d9d8d2ffa32329b36c0084b089e63
Nikole775 <nikole.labo@stud.ubbcluj.ro>
2025-03-27 10:14:42 +0200

    changed the designe for BorrowProduct
    Changed the designe and tryed to connect it to the database so I can test withh real example. Success

--- ArtAttack/App.xaml.cs
+++ ArtAttack/App.xaml.cs
@@ -1,55 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+﻿using Microsoft.UI.Xaml;
+using ArtAttack.Domain;
+using Microsoft.Data.SqlClient;
 
 namespace ArtAttack
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     public partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             this.InitializeComponent();
         }
 
-        /// <summary>
-        /// Invoked when the application is launched.
-        /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
-            m_window = new MainWindow();
-            //var bp = new BillingInfo();
-            //var ci = new CardInfo();
-            //var fp = new FinalisePurchase();
+            // Use your actual connection string
+            string connectionString = "Data Source=KORRAL775\\SQLEXPRESS;Initial Catalog=PurchaseDataBase;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate = True; Application Intent = ReadWrite; Multi Subnet FailOver = False";
+            //"DataSource =KORRAL775\SQLEXPRESS; Initial Catalog = PurchaseDataBase; Integrated Security = True; Connect Timeout = 30; Encript = true; Trust Server Certificate = true; Application Intent = ReadWrite; Multi Subnet FailOver = False;"
+            // Test with product ID 6 (available) or 7 (unavailable)
+            int productId = 6;
 
-            //m_window.Content = fp; // For testing the gui
-            m_window.Activate();
+            var window = new BorrowProductWindow(connectionString, productId);
+            window.Activate();
         }
-
-        private Window? m_window;
     }
 }
