1f6cc21e477c8ada238559c27a8c1e511c4caeba
Moldovan-Stefan-Iustin <146836085+Moldovan-Stefan-Iustin@users.noreply.github.com>
2025-03-27 15:34:27 +0200

    Am ViewModel :D

---
+++ ArtAttack/ViewModel/NotificationViewModel.cs
@@ -0,0 +1,135 @@
+﻿using ArtAttack.Domain;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ArtAttack.ViewModel
+{
+    public class NotificationViewModel : INotifyPropertyChanged
+    {
+        private readonly NotificationDataAdapter _dataAdapter;
+        private ObservableCollection<Notification> _notifications;
+        private int _unreadCount;
+        private bool _isLoading;
+        private int currentUserId;
+        private string connectionString = "Server=IUSTINS_LAPTOP\\SQLEXPRESS;Database=Notifications;Integrated Security=True;Encrypt=False;";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public NotificationViewModel(int currentUserId)
+        {
+            _dataAdapter = new NotificationDataAdapter(connectionString);
+            Notifications = new ObservableCollection<Notification>();
+            this.currentUserId = currentUserId;
+            MarkAsReadCommand = new NotificationRelayCommand<int>(async (id) => await MarkAsReadAsync(id));
+            _ = LoadNotificationsAsync(currentUserId);
+        }
+
+        public ObservableCollection<Notification> Notifications
+        {
+            get => _notifications;
+            set
+            {
+                _notifications = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int UnreadCount
+        {
+            get => _unreadCount;
+            set
+            {
+                _unreadCount = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(unReadNotificationsCountText));
+            }
+        }
+
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set
+            {
+                _isLoading = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand MarkAsReadCommand { get; }
+
+        public async Task LoadNotificationsAsync(int recipientId)
+        {
+            try
+            {
+                IsLoading = true;
+                var notifications = await Task.Run(() => _dataAdapter.GetNotificationsForUser(recipientId));
+
+                Notifications = new ObservableCollection<Notification>(notifications);
+                UnreadCount = Notifications.Count(n => !n.getIsRead());
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error loading notifications: {ex.Message}");
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+
+        public async Task MarkAsReadAsync(int notificationId)
+        {
+            try
+            {
+                await Task.Run(() => _dataAdapter.MarkAsRead(notificationId));
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error marking notification as read: {ex.Message}");
+            }
+        }
+
+        public async Task AddNotificationAsync(Notification notification)
+        {
+            if (notification == null)
+                throw new ArgumentNullException(nameof(notification));
+
+            try
+            {
+                await Task.Run(() => _dataAdapter.AddNotification(notification));
+
+                if (notification.getRecipientID() == currentUserId)
+                {
+                    Notifications.Insert(0, notification);
+                    UnreadCount++;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error adding notification: {ex.Message}");
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public string unReadNotificationsCountText
+        {
+            get => "You've got #" + _unreadCount + " unread notifications.";
+        }
+
+        private void UpdateUnreadCount()
+        {
+            UnreadCount = Notifications.Count(n => !n.getIsRead());
+            OnPropertyChanged(nameof(unReadNotificationsCountText));
+        }
+
+    }
+}
