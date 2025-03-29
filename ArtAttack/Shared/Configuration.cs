aaae2e8cde5fae2a2e156ad8100ae14151f3fa38
Darius Timoce <74290441+MadeOfDye@users.noreply.github.com>
2025-03-29 17:32:25 +0200

    Configuration Placed Back in Source Control

---
+++ ArtAttack/Shared/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Windows.ApplicationModel.Contacts;
+
+namespace ArtAttack.Shared
+{
+    public class Configuration
+    {
+        public static string _CONNECTION_STRING_ = "Data Source=DESKTOP-2F4KVKB;Initial Catalog=TestArtAttack;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    }
+}
